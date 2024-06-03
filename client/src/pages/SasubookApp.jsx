1ae3376108aea1d4a7f02fb852db928f870c128b
Rhonal Gonzalez <rhonalgnzl@gmail.com>
2024-06-03 18:04:52 -0300

    Cleaning unused components.

--- client/src/pages/SasubookApp.jsx
+++ client/src/pages/SasubookApp.jsx
@@ -1,22 +1,17 @@
 import '../App.css';
-import { useState, useEffect, useContext } from 'react';
+import { useState, useContext } from 'react';
 import axios from 'axios';
 
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { UsersPage } from './UsersPage/'
-import { UserPage } from './UserPage'
 import { UserFileFormPage } from './UserFileFormPage'
 import { Navigation } from '../components/Navigation'
 import { Toaster } from 'react-hot-toast'
-import toast from 'react-hot-toast'
-import { show_success_toast, show_error_toast } from '../utils/myToast';
+import { show_success_toast } from '../utils/myToast';
 import { storage } from '../utils/storage';
 import { MatchAllRoute } from './MatchAllRoute';
-// import { RegisterForm } from '../components/RegisterForm';
 import RegisterForm from '../components/RegisterForm';
-// import { LoginForm } from '../components/LoginForm'
 import LoginForm from '../components/LoginForm';
-// import { UserContext } from '../contexts/UserContext';
 import UserContext from '../contexts/UserContext';
 import Nav from '../components/Nav';
 
@@ -54,29 +49,23 @@ function SasubookApp(){
     if (currentUser) {
         return (
             <div>
-            {/* <div className="min-h-full max-h-full h-screen flex items-center justify-center mt-1 py-2 px-2 sm:px-6 lg:px-8"> */}
             <div className="min-h-full max-h-full h-screen flex justify-center mt-1 py-2 px-2 sm:px-6 lg:px-8">
                 <div className="max-w-full w-full space-y-8">
                     <BrowserRouter>
                     <Nav currentUser={currentUser} submitLogout={submitLogout}/>
-                    {/* <Navigation currentUser={currentUser} submitLogout={submitLogout}/> */}
                         <Routes>
-                        {/* <Route path="/" element={<LoginPage/>} />
-                        <Route path="/register" element={<RegisterPage/>} /> */}
-                        <Route path='/user'element={ <UsersPage/> }/>
-                        <Route path='/convert_pdf' element={ <UserFileFormPage/> }/>
-                        <Route path="*" element={<MatchAllRoute />} />
+                            <Route path='/user'element={ <UsersPage/> }/>
+                            <Route path='/convert_pdf' element={ <UserFileFormPage/> }/>
+                            <Route path="*" element={<MatchAllRoute />} />
                         </Routes>
                         <div className="flex justify-center w-full">
                         <h2>Usuario actual: {email}</h2>
-                        {/* {console.log(axios.get('http://127.0.0.1:8000/sasubook/user'))} */}
                         </div>
                         <Toaster/>
                     </BrowserRouter>
                 </div>
                 </div>
             </div>
-            // </div>
         );
     }
     return (
@@ -85,14 +74,7 @@ function SasubookApp(){
                 <div className="max-w-xl w-full space-y-8">
                     <BrowserRouter>
                     <Navigation />
-                        {/* <Routes> */}
-                            {/* <Route path="/" element={<LoginPage/>} />
-                            <Route path="/register" element={<RegisterPage/>} /> */}
-                            {/* <Route path='/convert_pdf' element={ <UserFileFormPage/> }/> */}
-
-                        {/* </Routes> */}
                         <Toaster/>
-
                     {
                         registrationToggle ? (
                             <RegisterForm update_form_btn={update_form_btn}></RegisterForm>
