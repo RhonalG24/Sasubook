f2412521c26938873e76987a4a2441b5182de59b
Rhonal Gonzalez <rhonalgnzl@gmail.com>
2024-08-26 21:34:41 -0300

    logo added

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function ElectronMainMethod(){
     const launchWindow = new BrowserWindow({
         title: "SasuBook",
         width: 777,
-        height: 444
+        height: 444,
+        icon: __dirname + '/client/public/SasuBook-Logo.png',
     });
 
     const appURL = "http://localhost:5173";
--- index_admin.js
+++ index_admin.js
@@ -4,11 +4,13 @@ function ElectronMainMethod(){
     const launchWindow = new BrowserWindow({
         title: "SasuBook Admin",
         width: 777,
-        height: 444
+        height: 444,
+        icon: __dirname + '/client/public/SasuBook-Logo.png',
     });
 
     const appURL = "http://localhost:8000/admin";
 
+    launchWindow.setOverlayIcon(nativeImage.createFromPath('/client/public/SasuBook-Logo.png'), 'SasuBook logo')
     launchWindow.loadURL(appURL);
 
 }
